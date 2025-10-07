fd893f71b0053ea8c499c154f99adaf1736971f5
Izana77 <yassinehassein@gmail.com>
2025-10-07 13:25:42 +0100

    2026

--- CS1603_WK1/src/Task1.java
+++ CS1603_WK1/src/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("Hello class of 2025!");
+	System.out.println("Hello class of 2026!");
 
 	
 	
