d6abac66ca2644860b506fa5c901286c589b85ae
Yassine Hassein <yassinehassein@gmail.com>
2025-10-14 12:29:56 +0100

    Add files via upload

---
+++ CS1603_WK3/src/Runtime2.java
@@ -0,0 +1,18 @@
+
+public class Runtime2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x; 
+		x = 100; 
+		System.out.println(x); 
+
+		x = x + 10; //Line A 
+		System.out.println(x); 
+
+		x = x + 20; //Line B 
+		System.out.println(x); 
+	}
+
+}
