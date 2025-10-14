9e2df8c1af13ba2ebde6e179f3a71c54db95dd2f
Yassine Hassein <yassinehassein@gmail.com>
2025-10-14 13:37:07 +0100

    Add files via upload

---
+++ CS1603_WK3/src/Variables_Project.java
@@ -0,0 +1,28 @@
+
+public class Variables_Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a, g;
+		double b, c, pi, y;
+		boolean d;
+		String e, f, name, z;
+		char h;
+		long x;
+		
+		a = 100;
+		b = 2.3;
+		c = -52.2;
+		d = true;
+		e = "I am ";
+		f = " a student";
+		g = 0;
+		h = '!';
+		pi = 3.142;
+		name = "Yassine";
+		
+		System.out.println();
+	}
+
+}
