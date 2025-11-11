dedc7c9d6ab30672cab1dd64c9638a4205a6d67c
Yassine Hassein <yassinehassein@gmail.com>
2025-11-11 13:52:41 +0000

    Add files via upload

---
+++ CS1603_WK7_DMSBW/src/main/java/SwiftbotTest/Test.java
@@ -0,0 +1,12 @@
+package SwiftbotTest;
+
+import swiftbot.SwiftBotAPI;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwiftBotAPI.INSTANCE.move(100, 100, 1000);
+	}
+
+}
